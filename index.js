a07623a3bfb3d9fb1b57fe1891559cf055c0c7cc
Badan Ser <157625611+Zeta-XD@users.noreply.github.com>
2024-03-22 09:27:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,68 @@
 const express = require('express');
 const axios = require('axios');
+const cheerio = require("cheerio");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-async function fetchGitHubData(user) {
-    try {
-        const response = await axios.get(`https://api.github.com/users/${user}`);
-        return response.data;
-    } catch (error) {
-        throw error.response.data;
-    }
-}
-
-app.get('/api/info/githubstalk', async (req, res) => {
+app.get('/api/xvs', async (req, res) => {
     const user = req.query.user;
     if (!user) return res.json({ status: false, creator: 'sezraah', message: '[!] masukan parameter user' });
+    
+        return new Promise((resolve, reject) => {
+    const baseurl = "https://www.xnxx.com";
+    fetch(`${baseurl}/search/${user}/${Math.floor(Math.random() * 3) + 1}`, {
+      method: "get",
+    })
+      .then((res) => res.text())
+      .then((res) => {
+        let $ = cheerio.load(res, {
+          xmlMode: false,
+        });
+        let title = [];
+        let url = [];
+        let desc = [];
+        let results = [];
 
-    try {
-        const gitstalk = await fetchGitHubData(user);
-        if (gitstalk.message === 'Not Found') return res.json({ status: false, message: 'User not found' });
-
-        res.json({ status: true, creator: 'zetahhh', result: gitstalk });
-    } catch (error) {
-        console.error('Error fetching GitHub data:', error);
-        res.status(500).json({ status: false, message: 'Internal server error' });
-    }
+        $("div.mozaique").each(function (a, b) {
+          $(b)
+            .find("div.thumb")
+            .each(function (c, d) {
+              url.push(
+                baseurl + $(d).find("a").attr("href").replace("/THUMBNUM/", "/")
+              );
+            });
+        });
+        $("div.mozaique").each(function (a, b) {
+          $(b)
+            .find("div.thumb-under")
+            .each(function (c, d) {
+              desc.push($(d).find("p.metadata").text());
+              $(d)
+                .find("a")
+                .each(function (e, f) {
+                  title.push($(f).attr("title"));
+                });
+            });
+        });
+        for (let i = 0; i < title.length; i++) {
+          results.push({
+            title: title[i],
+            info: desc[i],
+            link: url[i],
+          });
+        }
+        resolve({
+          creator: "baadan",
+          code: 200,
+          status: true,
+          result: results,
+        });
+      })
+      .catch((err) => reject({ code: 503, status: false, result: err }));
+  });
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
